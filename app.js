e1572161ea4e2b48f151f668815d3eddd91716b6
Trend20 <enockomondi305@gmail.com>
2020-06-21 11:23:36 -0400

    funcs

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const myCanvas = document.getElementById('myCanvas');
 const ctx = myCanvas.getContext('2d');
 
-ctx.fillStyle = '#222';
-ctx.fillRect(10,10,150,100);
+ctx.fillStyle = 'red';
+ctx.fillRect(20,20,350,300);
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,300,250);
+ctx.fillStyle = 'blue';
+ctx.fillRect('5,5,100,100');
