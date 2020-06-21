64a67331cd285bd6de1481a8d5e82121489fccd7
Trend20 <enockomondi305@gmail.com>
2020-06-21 12:08:22 -0400

    third canvas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const myCanvas = document.getElementById('myCanvas');
 const ctx = myCanvas.getContext('2d');
 
+
+// first figure
 // ctx.fillStyle = 'red';
 // ctx.fillRect(20,20,350,300);
 // ctx.fillStyle = 'green';
@@ -8,6 +10,7 @@ const ctx = myCanvas.getContext('2d');
 // ctx.fillStyle = 'blue';
 // ctx.fillRect('5,5,100,100');
 
+// second figure
 //    ctx.fillStyle='rgb(0,0,255)'; //Sets the color used for filling an area 
 //    ctx.fillRect(0,0,400,400);   //Fills a rectangle positioned at x and y, with a width and height of w and h.
 //    ctx.fillStyle='rgb(255,0,0)';  
@@ -16,3 +19,14 @@ const ctx = myCanvas.getContext('2d');
 //    ctx.fillRect(100,100,200,200);
 //    ctx.fillStyle='rgb(100,100,100)';  
 //    ctx.fillRect(125,175,150,25);
+
+
+// third figure
+    ctx.fillStyle='rgb(0,0,255)'; 
+    ctx.fillRect(30,30,300,300);           
+    ctx.fillStyle='rgba(0,255,0,0.5)';  
+    ctx.fillRect(60,60,300,300);    
+    ctx.fillStyle='rgba(255,0,0,0.25)';  
+    ctx.fillRect(90,90,300,300);
+    ctx.fillStyle='rgba(255,0,0,0.75)';  
+    ctx.fillRect(120,120,300,300); 
